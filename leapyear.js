9de1593f5f8747a5b0ac9094848f600bf8e75dca
Korede Adekola <korylinemedia@gmail.com>
2025-08-13 15:26:43 +0100

    Update and rename leapyear.html to leapyear.js

---
+++ leapyear.js
@@ -0,0 +1,8 @@
+var year = prompt(`Enter a year`);
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      console.log(`${year} is a leap year`);
+      alert(`${year} is a leap year`);
+    } else {
+      console.log(`${year} is not a leap year`);
+      alert(`${year} is not a leap year`);
+    }
