f921e225f5c9ea2514787b88d0d08b8014a4a11d
Korede Adekola <korylinemedia@gmail.com>
2025-08-13 15:37:35 +0100

    Update and rename fizzbuzz.html to fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body></body>
-  <script>
-    var output = [];
+var output = [];
     var count = 1;
     function fizzBuzz() {
       while (count <= 100) {
@@ -26,5 +17,3 @@
       }
     }
     fizzBuzz();
-  </script>
-</html>
